a12129d37aa525cfe8d41adc61e5a90971c0b5a8
lucileu <lucileu@icloud.com>
2020-03-19 16:51:32 +0800

    ”云存储“

--- learnCloud/miniprogram/pages/y-database/y-database.js
+++ learnCloud/miniprogram/pages/y-database/y-database.js
@@ -1,66 +1,53 @@
 // pages/y-database/y-database.js
+
+// 1.获取数据库对象
+const db = wx.cloud.database();
+
+// 2.获取操作的集合
+const collection = db.collection("students");
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  addDataToDB: function() {
+    collection.add({
+      data: {
+        name: "tom",
+        age: 28,
+        height: 1.88,
+        courses: ["现在经济", "统计学", "现代文学"],
+        goodFriend: {
+          name: "hanmeimei",
+          age: 20
+        },
+        location: db.Geo.Point(112, 23),
+        birth: new Date("1995-05-05")
+      }
+    }).then(console.log)
+      .catch(console.log)
+  },
+  
+  removeDataToDB: function() {
+    collection
+      .doc("d7e7dede5e7318fb0004ed483b4795f2")
+      .remove()
+      .then(console.log)
+      .catch(console.log)
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  updataDataToDB: function () {
+    collection
+      .doc("255a5c90-a1eb-4741-bd3b-e91c674015a5")
+      .set({
+        data: {
+          score: 100,
+          age: 25
+        }
+      })
+      .then(console.log)
+      .catch(console.log)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  queryDataToDB: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
